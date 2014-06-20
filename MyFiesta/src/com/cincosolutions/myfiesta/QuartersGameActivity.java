83b6dc1a4add1a57375f485a4574d84b1d276b89
297Hacker <nigelmaduroxxx@hotmail.com>
2014-06-20 22:17:10 +0200

    games

---
+++ MyFiesta/src/com/cincosolutions/myfiesta/QuartersGameActivity.java
@@ -0,0 +1,24 @@
+package com.cincosolutions.myfiesta;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class QuartersGameActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// Full Screen
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+		setContentView(R.layout.activity_quarters_game);
+
+		
+	}
+
+}
